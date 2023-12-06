021adb0e110a825574420dc290568156bb0b4878
DrFaustus1 <120943454+DrFaustus1@users.noreply.github.com>
2023-12-06 23:05:15 +0300

    Update Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -14,49 +14,57 @@ struct Person {
 int answer;
 long size_pred;
 
-int main()
-{
-	ifstream f_request;
-	ofstream f_answer;
-	setlocale(LC_ALL, "rus");
-	const char* nameRequest = "D:\REQUEST.txt";
-	const char* nameAnswer = "D:\ANSWER.txt";
+int main() {
+	setlocale(LC_ALL, "ru");
+	ifstream fin;
+	ofstream fout;
+	const char* nameA = "D:/answer.txt";
+	const char* nameR = "D:/request.txt";
 
-	cout << "server is working" << endl;
+	fout.open(nameA, ios::trunc);
+	fout.close();
+	fout.open(nameR, ios::trunc);
+	fout.close();
 
-	f_request.open(nameRequest, ios::binary);
-	f_request.seekg(0, ios::end);
-	size_pred = f_request.tellg();
+	fin.open(nameR);
+	fin.seekg(0, ios::end);
+	long size_pred = fin.tellg();
+	fin.close();
 
-	f_request.close();
+	cout << "Server is working" << endl;
 
-	//начало работы сервера 
 	while (true) {
-		f_request.open(nameRequest, ios::binary);
-		f_request.seekg(0, ios::end);
-		while (size_pred >= f_request.tellg()) {
-			Sleep(100);
-			f_request.seekg(0, ios::end);
+		int answer;
+		try {
+			fin.open(nameR);
 		}
-		f_request.seekg(size_pred, ios::beg);
-		f_request.read((char*)&Stud, sizeof(Stud));
-		size_pred = f_request.tellg();
-		f_request.close();
+		catch (exception& e) {
+			cout << "Error! Ошибка чтения!" << endl;
+			continue;
+		}
+		fin.seekg(0, ios::end);
+		while (size_pred >= fin.tellg()) {
+			Sleep(100);
+			fin.seekg(0, ios::end);
+		}
 
-		double IMT = (Stud.diffury + Stud.physics+ Stud.maths + Stud.oop)/4;
+
+		fin.seekg(size_pred, ios::beg);
+		fin.read((char*)&Stud, sizeof(Stud));
+		size_pred = fin.tellg();
+		fin.close();
+
+		double IMT = (Stud.diffury + Stud.physics + Stud.maths + Stud.oop) / 4;
 		if (IMT <= 3) answer = 0;
 		if (IMT > 3 && IMT < 4) answer = 1;
 		if (IMT >= 4) answer = 2;
 
-		answer = 2;
 
-		f_answer.open(nameAnswer, ios::binary || ios :: app);
-		f_answer.write(reinterpret_cast<const char*>(&answer), sizeof(answer));
-		//f_answer.write((char*)&answer, sizeof(answer));
-
-		f_answer.close();
-
+		fout.open(nameA, ios::app);
+		fout.write((char*)&answer, sizeof(answer));
+		fout.close();
+		cout << "Имя: " << Stud.name << endl << "Диффуры: " << Stud.diffury << endl << "Физика: " << Stud.physics << endl << "Матан: " << Stud.maths << endl << "ООП: " << Stud.oop;
 	}
-	
+	return 0;
 }
 
